828948fb698018d533b0662391a7c6566c821b78
EvgeniyTel <zheniatelegin1995@yandex.ru>
2023-02-16 13:24:25 +0300

    HW14.02_1

---
+++ HW_14.02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+Console.WriteLine("Введите трехзначное число:");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+int lastNum = Num % 100;
+
+int res = lastNum / 10;
+
+Console.WriteLine($"Вторая цифра в числе {res}" );*/
