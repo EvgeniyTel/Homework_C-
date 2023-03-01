328a5a1f3b94d2d6871c4bd3db4c10ae4ac31fdd
EvgeniyTel <zheniatelegin1995@yandex.ru>
2023-03-01 19:57:13 +0300

    Task_36

--- HW_24.02/Program.cs
+++ HW_24.02/Program.cs
@@ -32,3 +32,41 @@ int[] GetEvenValueInArray(int[] array)
     return rezult;
 }*/
 
+/*Задача 36: Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+
+int[] array = FillArray(4, 1, 200);
+
+Console.WriteLine($"В массиве: [{string.Join(" ", array)}]");
+Console.WriteLine(
+    $"Сумма элементов на нечетных нечетных позициях = {GetSumUnfairPositionArray(array)}"
+);
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(min, max + 1);
+    }
+    return result;
+}
+
+int[] GetSumUnfairPositionArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        if (array[i] % 2 == 1)
+            sum += array[i];
+    }
+    return sum;
+}*/
+
+
