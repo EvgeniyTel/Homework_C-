63956fde0ec0d87004886785f0b709e121bf007a
EvgeniyTel <zheniatelegin1995@yandex.ru>
2023-03-08 15:14:27 +0300

    Task_47 add

---
+++ HW_3.03/Task_47/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+/*Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9*/
+
+double[,] GetArrNaturalNum(int rows, int rowsLenght, int genEvenNumb, int probabilityNegativ = 3)
+{
+    double[,] arr = new double[rows, rowsLenght];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < rowsLenght; j++)
+        {
+            arr[i, j] = Math.Round(rnd.NextDouble() * genEvenNumb, 2, MidpointRounding.ToZero);
+            if (rnd.Next(0, probabilityNegativ) == 0)
+            {
+                arr[i, j] *= -1;
+            }
+        }
+    }
+    return arr;
+}
+void PrintArr(double[,] arr)
+{
+    int rows = arr.GetLength(0);
+    int rowsLenght = arr.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        Console.Write($" ");
+        for (int j = 0; j < rowsLenght; j++)
+        {
+            Console.Write($"{arr[i, j]} |");
+        }
+        Console.WriteLine($"");
+    }
+}
+PrintArr(GetArrNaturalNum(3, 4, 10));
